62ddf7626b765c962de518b6f66a2c9ec737ae98
dimonwhite <gen-tver@yandex.ru>
2020-02-18 01:51:20 +0300

    feat: completed task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,33 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)) {
+        throw new Error;
+    }
+
+    let array = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        switch(arr[i]) {
+            case '--discard-next':
+                i++;
+                break;
+            case '--discard-prev':
+                array.pop();
+                break;
+            case '--double-next':
+                if( i + 1 < arr.length ) {
+                    array.push(arr[i + 1]);
+                }
+                break;
+            case '--double-prev':
+                if( i - 1 >= 0 ) {
+                    array.push(arr[i - 1]);
+                }
+                break;
+            default:
+                array.push(arr[i]);
+                break;
+        }
+    }
+
+    return array;
 };
