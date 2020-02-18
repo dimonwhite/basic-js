21463cfda9ea09aafe5a1ac729be333305c0a812
dimonwhite <gen-tver@yandex.ru>
2020-02-19 01:38:24 +0300

    feat: completed task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,36 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    str = String(str);
+
+    let repeatTimes = options.repeatTimes ? options.repeatTimes : 1,
+        separator = options.separator ? options.separator : '+',
+        addition = options.addition !== undefined ? String(options.addition) : false,
+        additionRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1,
+        additionSeparator = options.additionSeparator ? options.additionSeparator : '|',
+        arr = [];
+
+    if(typeof repeatTimes != 'number') {
+        repeatTimes = 1;
+    }
+
+    if(typeof additionRepeatTimes != 'number') {
+        additionRepeatTimes = 1;
+    }
+
+    let string = str;
+
+    if (addition) {
+        string += addition;
+        for(let i = 1; i < additionRepeatTimes; i++) {
+            string += additionSeparator + addition;
+        }
+    }
+
+    let result = string;
+
+    for(let i = 1; i < repeatTimes; i++) {
+        result += separator + string;
+    }
+
+    return result;
 };
   
