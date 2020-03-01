68e00b51201e63374085a7f4fbb449ee0c77d9a9
dimonwhite <gen-tver@yandex.ru>
2020-03-01 17:40:38 +0300

    feat: completed task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,70 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(reverse) {
+        this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+        this.reverse = reverse;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if (!message || !key)
+            throw new Error();
+
+        message = message.toUpperCase().split("");
+        key = key.toUpperCase().split("");
+
+        let i = 0;
+        key = message.reduce((accum, symbol) => {
+            if( i === key.length ) {
+                i = 0;
+            }
+            let item;
+            if ( this.alphabet.indexOf(symbol) !== -1 ) {
+                item = key[i];
+                i++;
+            }
+            else {
+                item = '';
+            }
+            return [...accum, item];
+        }, []);
+
+        let result = message.reduce((accum, item, i) => {
+            let symbol = this.alphabet.indexOf(item) !== -1 ? this.alphabet[(this.alphabet.indexOf(item) + this.alphabet.indexOf(key[i])) % this.alphabet.length] : item;
+            return [...accum, symbol];
+        }, []);
+
+        return this.reverse === false ? result.reverse().join("") : result.join("");
+    }
+
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key)
+            throw new Error();
+
+        encryptedMessage = encryptedMessage.toUpperCase().split("");
+        key = key.toUpperCase().split("");
+
+        let i = 0;
+        key = encryptedMessage.reduce((accum, symbol) => {
+            if( i === key.length ) {
+                i = 0;
+            }
+            let item;
+            if ( this.alphabet.indexOf(symbol) !== -1 ) {
+                item = key[i];
+                i++;
+            }
+            else {
+                item = '';
+            }
+            return [...accum, item];
+        }, []);
+
+        let result = encryptedMessage.reduce((accum, item, i) => {
+            let symbol = this.alphabet.indexOf(item) !== -1 ? this.alphabet[Math.abs(this.alphabet.indexOf(item) + this.alphabet.length - this.alphabet.indexOf(key[i])) % this.alphabet.length] : item;
+            return [...accum, symbol];
+        }, []);
+
+        return this.reverse === false ? result.reverse().join("") : result.join("");
+
     }
 }
 
